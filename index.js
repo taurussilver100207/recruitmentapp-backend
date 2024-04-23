991abb96cd84b47d17e1bf94517c4ba0b2636006
LongNTDev <codeinlife10@example.com>
2024-04-23 20:15:23 +0700

    fix: change log

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ app.listen(PORT, () => console.log(`Running in port ${PORT}`))
 
 console.log("Hello World");
 console.log("Override something!!!");
-console.log('hehehe');
+console.log('hehehe thay đổi 1 chutts');
