04b1870b1a0b38fdd527c1eb1e5a9720cb8e2f8b
taurussilver100207 <110682916+taurussilver100207@users.noreply.github.com>
2024-04-23 19:55:26 +0700

    a

--- routes/userprofile.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getUserProfile } from "../controllers/userprofile.js"
-import { verifyToken } from "../middleware/auth.js"
-const router = express.Router()
-
-router.get("/:userId", verifyToken, getUserProfile)
-
-export default router
